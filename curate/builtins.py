ba058b3ab89e914697c03adba87d6371a05c96b6
William Waites <ww@styx.org>
2010-12-04 18:04:48 +0100

    documentation...

--- curate/builtins.py
+++ curate/builtins.py
@@ -3,8 +3,15 @@ from logging import getLogger
 from rdflib.Graph import ConjunctiveGraph
 from rdflib.store.SPARQL import SPARQLStore
 
-def httpGET(pats, pato):
-    log = getLogger("httpGET(%s, %s)" % (pats.n3(), pato.n3()))
+def httpGET(_unusedp, resourcep):
+    """
+    :param resourcep: resource pattern, the bound version of which
+        checked
+
+    Check if the given resource responds with data to an HTTP
+    GET request.
+    """
+    log = getLogger("httpGET(%s, %s)" % (_unusedp.n3(), resourcep.n3()))
     log.debug("init")
     def f(_unused, resource):
         try:
@@ -18,8 +25,18 @@ def httpGET(pats, pato):
             return False
     return f
 
-def sparqlCheck(pats, pato):
-    log = getLogger("sparqlCheck(%s, %s)" % (pats.n3(), pato.n3()))
+def sparqlCheck(_unusedp, endpointp):
+    """
+    :param endpointp: endpoint pattern, the bound version of which
+        will be checked
+
+    Check if the SPARQL endpoint functions. In this context, to
+    *function* means returning a result when given a simple query.
+    The query used is:
+
+        SELECT * WHERE { ?s ?p ?o } LIMIT 1
+    """
+    log = getLogger("sparqlCheck(%s, %s)" % (_unusedp.n3(), endpointp.n3()))
     log.debug("init")
     def f(_unused, endpoint):
         store = SPARQLStore(endpoint)
