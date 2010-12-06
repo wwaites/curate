26714a61163d2ea8ec114cc159f40a0be4f1f164
William Waites <ww@styx.org>
2010-12-06 18:17:55 +0100

    Bump version and add write operations back to the API, and docs...

--- curate/builtins.py
+++ curate/builtins.py
@@ -54,12 +54,32 @@ def sparqlCheck(_unusedp, endpointp):
     return f
 
 def addTag(datasetp, tagp):
+    """
+    :param datasetp: the dataset variable, the bound version of which
+        will acquire the tag
+    :param tagp: the tag variable, the bound version of which will be
+        added to the dataset
+
+    This builtin adds a tag to a dataset via the CKAN api. The tag is
+    expected to be a literal value.
+    """
     def f(dataset, tag):
         queue.add(dataset, "tags", [unicode(tag)])
         return True
     return f
 
 def addGroup(datasetp, groupp):
+    """
+    :param datasetp: the dataset variable, the bound version of which
+        will be added to the specified group
+    :param groupp: the group variable, the bound version of which will
+        have the dataset added to.
+
+    This builtin adds a dataset to a group. The group can be a  URI or
+    a literal. If it is a URI, it will be split on the / character and
+    the dataset will be added to the group named with the last component
+    of the path.
+    """
     def f(dataset, group):
         if isinstance(group, URIRef):
             _, group = group.rsplit("/", 1)
--- curate/command.py
+++ curate/command.py
@@ -1,3 +1,51 @@
+"""
+.. program:: curate
+
+Tool for curation of CKAN datasets. It reads RDF descriptions
+of the dataset and applies the specified rules. Inferred 
+statements are written to the standard output in Notation 3
+and built-in predicates may be used to check specific conditions
+or perform certain actions.
+
+Usage: ``curate [options] [dataset [dataset [...]]]``
+
+.. cmdoption:: -b base_uri
+
+    Base URI to read dataset descriptions from. The RDF graph
+    at ${base_uri}/${dataset} is read for each dataset and 
+    passed as input to the inference rules.
+
+.. cmdoption:: -a api_base
+
+    Base URI for calls to the CKAN API. This is useful for
+    running the tool against different CKAN instances.
+
+.. cmdoption:: -r rules
+
+    Inference rules in Notation 3 to read. May be specified
+    multiple times
+
+.. cmdoption:: -k api_key
+
+    CKAN API Key to use where authentication is required
+
+.. cmdoption:: -s
+
+    Save any inferred dataset metadata to the CKAN instance.
+    If this is not specified the program will run and perform all
+    checks but will not actually perform any operations via the
+    CKAN api.
+
+.. cmdoption:: -v
+
+    Verbose output
+
+.. cmdoption:: -h, --help
+
+    output a help message
+
+"""
+
 import argparse
 
 import warnings
@@ -10,7 +58,13 @@ from FuXi.Rete.Util import generateTokenSet
 from curate.work import queue
 
 def curate():
-    parser = argparse.ArgumentParser(description=__doc__)
+    parser = argparse.ArgumentParser(description="""
+Tool for curation of CKAN datasets. It reads RDF descriptions
+of the dataset and applies the specified rules. Inferred 
+statements are written to the standard output in Notation 3
+and built-in predicates may be used to check specific conditions
+or perform certain actions.
+""")
     parser.add_argument("-b", dest="base", default="http://semantic.ckan.net/package/",
                         help="RDF description base URI to look for packages to consider")
     parser.add_argument("-r", dest="rules", action="append", default=[], 
